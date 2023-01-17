0836b97e01f195efefe83a8b81844c55a93477b7
Shlomi Israely <sijproject@gmail.com>
2023-01-17 18:40:11 +0100

    added separation anxiety

--- src/config/config.js
+++ src/config/config.js
@@ -13,7 +13,8 @@ import {
   faShoppingCart,
   faUserShield,
   faShieldAlt,
-  faHotel
+  faHotel,
+  faDoorOpen
 } from "@fortawesome/free-solid-svg-icons";
 
 import { faFacebookF } from "@fortawesome/free-brands-svg-icons";
@@ -35,10 +36,11 @@ export const ARTICLES_URL =
 
 export const PROFESSIONALS_DB_TYPES = {
   trainers: { label: "אימון כלבים רגישים", icon: faDog },
-  puppies: { label: "אימון גורים", icon: faPaw },
   walkers: { label: "הולכת כלבים", icon: faWalking },
   "dog hotels": { label: "פנסיון", icon: faHotel },
   sitters: { label: "סיטינג", icon: faHome },
+  puppies: { label: "אימון גורים", icon: faPaw },
+  separation: { label: "חרדת נטישה", icon: faDoorOpen },
   groomers: { label: "טיפוח כלבים", icon: faCut },
   veterinarians: { label: "וטרינריה", icon: faBriefcaseMedical },
   Shops: { label: "חנויות", icon: faShoppingCart },
